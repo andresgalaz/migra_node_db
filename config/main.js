f98553983b65294fab2e11ee7582b2c69852dc86
Andres Galaz <andres.galaz@gmail.com>
2016-11-24 17:10:58 -0300

    ajuste -3 horas B.A.

--- config/main.js
+++ config/main.js
@@ -1,25 +1,43 @@
+var oConexion = null, cDirAdjunto = null;
+if (process.env.WSAPI_AMBIENTE == 'DESA') {
+	cDirAdjunto = '/home/agalaz/adjunto';
+	oConexion = {
+		host : '127.0.0.1', // your host
+		user : 'snapcar', // your database user
+		password : 'snapcar', // your database password
+		database : 'score_desa',
+		charset : 'UTF8_GENERAL_CI'
+	};
+} else if (process.env.WSAPI_AMBIENTE == 'PROD') {
+	cDirAdjunto = '/home/ubuntu/adjunto/';
+	oConexion = {
+		host : '127.0.0.1', // your host
+		user : 'snapcar', // your database user
+		password : 'snapcar', // your database password
+		database : 'score',
+		charset : 'UTF8_GENERAL_CI'
+	};
+}
+
 var dbLocal = require('knex')({
-	client: 'mysql',
-	connection: {
-		host: '127.0.0.1',
-		user: 'snapcar',
-		password: 'snapcar',
-		database: 'score_desa',
-		charset: 'UTF8_GENERAL_CI'
-	}
+	client : 'mysql',
+	connection : oConexion
 });
 var dbRemota = require('knex')({
-	client: 'mysql',
-	connection: {
-		host: '7puentes',
-		user: 'snapcar',
-		password: 'oycobe',
-		database: 'snapcar',
-		charset: 'UTF8_GENERAL_CI'
+	client : 'mysql',
+	connection : {
+		host : 'data.appcar.com.ar',
+		port : 23849,
+		user : 'snapcar',
+		password : 'oycobe',
+		database : 'snapcar',
+		charset : 'UTF8_GENERAL_CI'
 	}
 });
 
 module.exports = {
-  'dbRemota': dbRemota,
-  'dbLocal': dbLocal
+	'dbRemota' : dbRemota,
+	'dbLocal' : dbLocal,
+	'ambiente' : process.env.WSAPI_AMBIENTE,
+	'dirAdjunto' : cDirAdjunto
 };
--- proceso.js
+++ proceso.js
@@ -1,33 +1,12 @@
 // Import dependencies
 const _ = require('underscore-plus');
 const batch = require('batchflow');
+const config = require(',/config/main');
+const moment = require('moment');
 
 // Conexiones
-var dbLocal = null;
-var dbRemota = null;
-
-dbLocal = require('knex')({
-	client: 'mysql',
-	connection: {
-		host: '127.0.0.1',
-		user: 'snapcar',
-		password: 'snapcar',
-		database: 'score',
-		charset: 'UTF8_GENERAL_CI'
-	}
-});
-
-dbRemota = require('knex')({
-	client: 'mysql',
-	connection: {
-		host: 'data.appcar.com.ar',
-		user: 'snapcar',
-		port: 23849,
-		password: 'oycobe',
-		database: 'snapcar',
-		charset: 'UTF8_GENERAL_CI'
-	}
-});
+var dbLocal = config.dbLocal;
+var dbRemota = config.dbRemota ;
 
 // Guarda los eventos a insertar
 var arrEventos = [];
@@ -39,103 +18,94 @@ var nLastViaje = 0;
 var arrFunciones = [];
 
 // Toma el ID del último viaje acutalizado
-arrFunciones[ arrFunciones.length ] = function(fnNext) {
-	dbRemota
-	.select( 'id as trip_id', 'updated_at' )
-	.from('trips')
-	.orderBy( 'updated_at', 'desc' )
-	.limit( 1 )
-	.then(function(data){
-		console.log('UPDATED', data);
-		nLastViaje = data[0].trip_id;
-		nLastModif = data[0].updated_at;
-		fnNext();
-	}).catch(function(e){
+arrFunciones[arrFunciones.length] = function(fnNext) {
+	dbRemota.select('id as trip_id', 'updated_at').from('trips').orderBy('updated_at', 'desc').limit(1).then(
+			function(data) {
+				console.log('UPDATED', data);
+				nLastViaje = data[0].trip_id;
+				nLastModif = data[0].updated_at;
+				fnNext();
+			}).catch(function(e) {
 		fnNext(e);
 	});
 };
 
 // Verifica el ID del último viaje acutalizado
-arrFunciones[ arrFunciones.length ] = function(fnNext) {
-	dbLocal
-	.select( 'nIdViaje', 'tModif' )
-	.from('tEvento')
-	.orderBy( 'tModif', 'desc' )
-	.limit( 1 )
-	.then(function(data){
+arrFunciones[arrFunciones.length] = function(fnNext) {
+	dbLocal.select('nIdViaje', 'tModif').from('tEvento').orderBy('tModif', 'desc').limit(1).then(function(data) {
 		console.log('UPDATED', data);
-		if( data.length == 0 ) {
+		if (data.length == 0) {
 			console.log('Procesar Todo de nuevo');
 			nLastViaje = 0;
 			fnNext();
-		} else if( data[0].tModif < nLastModif ) {
+		} else if (data[0].tModif < nLastModif) {
 			console.log('Procesar');
 			fnNext();
 		} else
-			console.assert(false,'No hay nuevas actualizaciones');
+			console.assert(false, 'No hay nuevas actualizaciones');
 		// nLastViaje = data[0].trip_id
 		// fnNext();
-	}).catch(function(e){
+	}).catch(function(e) {
 		fnNext(e);
 	});
 };
 
 // Borra a partir del último viaje actualizado
-arrFunciones[ arrFunciones.length ] = function(fnNext) {
-	dbLocal('tEvento')
-	.where('nIdViaje','>=',nLastViaje)
-	.del()
-	.then(function(data){
+arrFunciones[arrFunciones.length] = function(fnNext) {
+	dbLocal('tEvento').where('nIdViaje', '>=', nLastViaje).del().then(function(data) {
 		console.log('DELETE', data);
 		fnNext();
-	})
-	.catch(function(e){
+	}).catch(function(e) {
 		fnNext(e);
 	});
 };
 
 // Lee eventos desde la base remota
-arrFunciones[ arrFunciones.length ] = function(fnNext) {
-	dbRemota
-	.select( 'trip_id', 'vehicle_id', 'driver_id','prefix','puntos' ,'obs_value' ,'permited_value' ,'obs_fecha' 
-		   , 'fecha_ini', 'fecha_fin', 'distance', 'calle', 'calle_inicio', 'latitude', 'longitude',  'ts_modif' )
-	.from('trip_observations_view')
+arrFunciones[arrFunciones.length] = function(fnNext) {
+	dbRemota.select('trip_id', 'vehicle_id', 'driver_id', 'prefix', 'puntos', 'obs_value', 'permited_value',
+			'obs_fecha', 'fecha_ini', 'fecha_fin', 'distance', 'calle', 'calle_inicio', 'latitude', 'longitude',
+			'ts_modif').from('trip_observations_view')
 	// Convierte arreglo de objetos a un arreglo de int
-	.where( 'trip_id', '>=', nLastViaje )
-	.orderBy( 'trip_id' )
-	.orderBy( 'obs_fecha' )
-	.then(function(data){
+	.where('trip_id', '>=', nLastViaje).orderBy('trip_id').orderBy('obs_fecha').then(function(data) {
 		var idTripActual = -1;
 		var idxIniViaje = -1;
+
+		// Ajusta fecha a Argentina/Buenos Aires -3
+		var tEventoBA = moment(itm.obs_fecha,'YYYY-MM-DD HH:mm:ss').subtract(3,'h');
+		if( tEventoBA.isValid())
+			tEventoBA = tEventoBA.format('YYYY-MM-DD HH:mm:ss');
+		else 
+			tEventoBA = null;
 		
 		// Convierte datos de la base remota a la base Local
-		_.each( data, function( itm, idx, arr ){
+		_.each(data, function(itm, idx, arr) {
 			var evento = {
-				nIdViaje         : itm.trip_id,
-				nIdTramo         : 1,
-				fUsuario		 : parseInt( itm.driver_id ),
-				fVehiculo		 : parseInt( itm.vehicle_id ),
-				tEvento          : itm.obs_fecha,
-				nLG              : itm.longitude,
-				nLT              : itm.latitude,
-				cCalle           : itm.calle,
+				nIdViaje : itm.trip_id,
+				nIdTramo : 1,
+				fUsuario : parseInt(itm.driver_id),
+				fVehiculo : parseInt(itm.vehicle_id),
+				tEvento : tEventoBA,
+				nLG : itm.longitude,
+				nLT : itm.latitude,
+				cCalle : itm.calle,
 				nVelocidadMaxima : itm.permited_value,
-				nValor           : itm.obs_value,
-				nPuntaje         : itm.puntos,
-				tModif			 : itm.ts_modif
+				nValor : itm.obs_value,
+				nPuntaje : itm.puntos,
+				tModif : itm.ts_modif
 			};
 			// Cambia el tipo de evento de A,E y F a 3, 5 y 4, respectivamente
-			evento.fTpEvento = [ 3, 4, 5 ][ _.indexOf(['A','F','E'], itm.prefix)];
+			evento.fTpEvento = [ 3, 4, 5 ][_.indexOf([ 'A', 'F', 'E' ], itm.prefix)];
 
 			// Lo agrega si está OK
-			if(  !isNaN(evento.fUsuario ) && evento.fUsuario  > 0
-              && !isNaN(evento.fVehiculo) && evento.fVehiculo > 0 ){
-				// Si hay camio de viaje, se cierra el anteior y se crea uno nuevo
-				if( idTripActual != itm.trip_id ){
+			if (!isNaN(evento.fUsuario) && evento.fUsuario > 0 && !isNaN(evento.fVehiculo) && evento.fVehiculo > 0) {
+				// Si hay camio de viaje, se cierra el anteior y se crea uno
+				// nuevo
+				if (idTripActual != itm.trip_id) {
 					idTripActual = itm.trip_id;
-					if( idxIniViaje >= 0 ){
-						// Crea fin del viaje del anterior, usa el último evento como referencia
-						var eventoFin = _.clone( _.last( arrEventos ));
+					if (idxIniViaje >= 0) {
+						// Crea fin del viaje del anterior, usa el último
+						// evento como referencia
+						var eventoFin = _.clone(_.last(arrEventos));
 						var itmIni = data[idxIniViaje];
 						eventoFin.fTpEvento = 2;
 						eventoFin.tEvento = itmIni.fecha_fin;
@@ -144,26 +114,28 @@ arrFunciones[ arrFunciones.length ] = function(fnNext) {
 						eventoFin.nVelocidadMaxima = 0;
 						arrEventos.push(eventoFin);
 					}
-					var eventoIni = _.clone( evento );
-					eventoIni.fTpEvento	 = 1;
-					eventoIni.tEvento    = itm.fecha_ini;
-					eventoIni.cCalle     = itm.calle_inicio;
+					var eventoIni = _.clone(evento);
+					eventoIni.fTpEvento = 1;
+					eventoIni.tEvento = itm.fecha_ini;
+					eventoIni.cCalle = itm.calle_inicio;
 
 					idxIniViaje = idx;
 					arrEventos.push(eventoIni);
 				}
-				if( evento.fTpEvento )
+				if (evento.fTpEvento)
 					arrEventos.push(evento);
 				// lastFecha = itm.obs_fecha;
 			} else {
 				// Evento fallido, falta información
-				console.log( evento );
+				console.log(evento);
 			}
+			;
 		});
 		// Cierra el ultimo viaje
-		if( idxIniViaje >= 0 ){
-			// Crea fin del viaje del anterior, usa el último evento como referencia
-			var eventoFin = _.clone( _.last( arrEventos ));
+		if (idxIniViaje >= 0) {
+			// Crea fin del viaje del anterior, usa el último evento como
+			// referencia
+			var eventoFin = _.clone(_.last(arrEventos));
 			var itmIni = data[idxIniViaje];
 			eventoFin.fTpEvento = 2;
 			eventoFin.tEvento = itmIni.fecha_fin;
@@ -173,20 +145,18 @@ arrFunciones[ arrFunciones.length ] = function(fnNext) {
 			arrEventos.push(eventoFin);
 		}
 		fnNext();
-	}).catch(function(e){
+	}).catch(function(e) {
 		fnNext(e);
 	});
 };
 
-
 // Inserta resulta en la tabla tEvento
-arrFunciones[ arrFunciones.length ] = function(fnNext) {
-	console.log( 'Insertando ', arrEventos.length, ' registros');
-	dbLocal.transaction( function(trx) {
-		dbLocal('tEvento').transacting(trx).insert( arrEventos )
-   		// .then(function(resp) { })
-    	.then(trx.commit)
-    	.catch(trx.rollback);
+arrFunciones[arrFunciones.length] = function(fnNext) {
+	console.log('Insertando ', arrEventos.length, ' registros');
+	dbLocal.transaction(function(trx) {
+		dbLocal('tEvento').transacting(trx).insert(arrEventos)
+		// .then(function(resp) { })
+		.then(trx.commit).catch(trx.rollback);
 
 	}).then(function(resp) {
 		console.log('Transaction complete.');
@@ -197,22 +167,19 @@ arrFunciones[ arrFunciones.length ] = function(fnNext) {
 	});
 };
 
-batch(arrFunciones).sequential()
-	.each( function( i, item, fnNext ){
-		console.log('Paso Nº ',i);
-		item(fnNext);
-	})
-	.error(function(e){
-        if( e.name == 'AssertionError' ){
-            console.log('FIN');
-        } else {
-            console.error('Salida con error', e.stack);
-            console.error(e);
-            console.log(e);
-        }
-		process.exit(0);
-	})
-	.end(function(){
+batch(arrFunciones).sequential().each(function(i, item, fnNext) {
+	console.log('Paso Nº ', i);
+	item(fnNext);
+}).error(function(e) {
+	if (e.name == 'AssertionError') {
 		console.log('FIN');
-		process.exit(0);
-	});
+	} else {
+		console.error('Salida con error', e.stack);
+		console.error(e);
+		console.log(e);
+	}
+	process.exit(0);
+}).end(function() {
+	console.log('FIN');
+	process.exit(0);
+});
